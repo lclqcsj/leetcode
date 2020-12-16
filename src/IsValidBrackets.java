356ce9dc8e5238f1937d97389bb403960b8a0926
lcl <1835881209@qq.com>
2020-12-16 23:52:31 +0800

    增加题目秒杀

--- src/IsValidBrackets.java
+++ src/IsValidBrackets.java
@@ -2,7 +2,7 @@ import java.util.Stack;
 
 /**
  * @author：licailin
- *
+ * 题目：有效的括号
  * 题目地址：https://leetcode-cn.com/problems/valid-parentheses/
  */
 public class IsValidBrackets {
@@ -26,7 +26,7 @@ public class IsValidBrackets {
                 stack.push(str.charAt(idx));
             } else {
                 // 遇到右边括号，队列为空，不满足，直接return false
-                if (stack.empty() && idx < leftStr.length()) {
+                if (stack.empty() && idx < str.length()) {
                     return false;
                 }
 
@@ -47,6 +47,6 @@ public class IsValidBrackets {
     }
 
     public static void main(String[] args){
-        System.out.println(IsValidBrackets.isValid("}[]()}"));
+        System.out.println(IsValidBrackets.isValid("(){}}{"));
     }
 }
--- src/MissMinNum.java
+++ src/MissMinNum.java
@@ -5,7 +5,7 @@
  * 解题思路：
     1、时间复杂度O（n*㏒n + O（n））
     2、先快速排序，获取到有序数列
-    3、顺序访问每个元素
+    3、顺序访问每个元素，做等差数列计算, 特别注意，是否只有一个元素的情况
  */
 public class MissMinNum {
 
@@ -42,20 +42,26 @@ public class MissMinNum {
     public static int missMinNum(int[] bbb) {
         int[] num = fastSort(bbb, 0, bbb.length - 1);
         int idx = 0;
+        boolean first = true;
+        if (num.length == 1) {
+            if (num[0] != 0) {
+                return 0;
+            }
+            return 1;
+        }
         while (idx < num.length - 1) {
             // 忽略负数
-            if (num[idx] <= 0) {
+            if (num[idx] < 0) {
                 idx ++;
                 continue;
             }
-
-            // 第一个大于0的正整数
-            if (num[idx] > 1 && (idx >= 1 && num[idx - 1] <= 0)) {
-                return 1;
+            // 第一个就是非0正整数{-2, -1, 0, 1, 2}
+            if (num[idx] > 0 && first) {
+                return 0;
             }
-
-            // 如果刚好等于1，判断下一位
-            if (idx + 1 < num.length - 1 && num[idx + 1] - num[idx] > 1) {
+            first = false;
+            // 判断是否是等差数列, 用下一位减去当前位
+            if (num[idx + 1] - num[idx] > 1) {
                 return num[idx] + 1;
             }
             idx ++;
@@ -64,7 +70,7 @@ public class MissMinNum {
     }
 
     public static void main(String[] args) {
-        int[] a = {-1, -2, 0};
+        int[] a = {-1, 0, 1};
         System.out.println(missMinNum(a));
     }
 }
